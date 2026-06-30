e2607dec395411de8494af647cebf51240540fe5
Mounaimqc <160617744+Mounaimqc@users.noreply.github.com>
2026-06-30 18:07:07 +0100

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,3 @@
-/**
- * SallePro - Firebase v11 Initialization Service
- */
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
@@ -19,6 +15,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
